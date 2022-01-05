a85ab2559fb6b414075453a5e0827956e44f663f
Obinna Uzozie <Uzozie.obi@icloud.com>
2022-01-05 21:35:31 +0800

    Included bottom navigation with React Tab navigation and reactnativenavigation

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
 import { ThemeProvider } from "styled-components/native";
-
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import {
   useFonts as useOswald,
   Oswald_400Regular,
 } from "@expo-google-fonts/oswald";
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
-
 import { theme } from "./src/infrastructure/theme";
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
+
+const Tab = createBottomTabNavigator();
+
+const Settings = () => <Text>Settings</Text>
+const Map = () => <Text>Map</Text>
+
+
+
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
@@ -27,7 +35,13 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <RestaurantsScreen />
+       <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+          <Tab.Screen name="Map" component={Map} />
+          <Tab.Screen name="Settings" component={Settings} />
+        </Tab.Navigator>
+       </NavigationContainer>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
