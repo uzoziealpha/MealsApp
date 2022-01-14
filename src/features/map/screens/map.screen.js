a4b47bb2586f679f73d50c1711e26aa2fb8ce648
Obinna Uzozie <Uzozie.obi@icloud.com>
2022-01-14 12:38:32 +0800

    Adding markers to the map ffunction using viewport and geometry from location

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -1,7 +1,9 @@
 import React, { useContext, useState, useEffect } from "react";
+import { View } from "react-native";
 import MapView from "react-native-maps";
 import styled from "styled-components/native";
-
+import { LocationContext } from "../../../services/location/location.context";
+import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
 import { Search } from "../components/search.component";
 
 const Map = styled(MapView)`
@@ -9,9 +11,38 @@ const Map = styled(MapView)`
    width: 100%;
 `;
 
-export const MapScreen = () => (
-     <>
-     <Search/>
-     <Map />
-     </>
-);
+export const MapScreen = () => {
+  const { location } = useContext(LocationContext);
+  const { restaurants = [] } = useContext(RestaurantsContext);
+
+  const [latDelta, setLatDelta] = useState(0);
+
+  const { lat, lng, viewport } = location;
+
+
+  useEffect(() => {
+     const northeastLat = viewport.northeast.lat;
+     const southwestLat = viewport.southwest.lat;
+
+     setLatDelta(northeastLat - southwestLat);  
+  }, [location, viewport]);
+  return (
+  <>
+    <Search />
+    <Map
+       region={{
+           latitude: lat,
+           longitude: lng,
+           latitudeDelta: latDelta,
+           longitudeDelta: 0.02,
+       }}
+    >
+        {restaurants.map((restaurant) => {
+            return <MapView.Marker>
+               
+            </MapView.Marker>;
+        })}
+    </Map>
+  </>
+  );
+};
