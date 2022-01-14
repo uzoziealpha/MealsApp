3e5fbd0941c4834691d04136ac7aa5261e7f8d63
Obinna Uzozie <Uzozie.obi@icloud.com>
2022-01-14 20:48:34 +0800

    Updated the map markers with images and title

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -1,15 +1,17 @@
 import React, { useContext, useState, useEffect } from "react";
-import { View } from "react-native";
 import MapView from "react-native-maps";
 import styled from "styled-components/native";
 import { LocationContext } from "../../../services/location/location.context";
 import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
 import { Search } from "../components/search.component";
+import { MapCallout} from "../components/map-callout.component";
+
 
 const Map = styled(MapView)`
    height: 100%;
    width: 100%;
 `;
+
 
 export const MapScreen = () => {
   const { location } = useContext(LocationContext);
@@ -38,14 +40,20 @@ export const MapScreen = () => {
        }}
     >
         {restaurants.map((restaurant) => {
-            return <MapView.Marker
+            return (
+            <MapView.Marker
              key={restaurant.name}
              title={restaurant.name}
              coordinate={{
                 latitude: restaurant.geometry.location.lat,
                 longitude: restaurant.geometry.location.lng,
              }}
-            />;
+        >
+              <MapView.Callout>
+                <MapCallout restaurant={restaurant} />
+              </MapView.Callout>
+            </MapView.Marker>
+            );
         })}  
     </Map>    
   </>
