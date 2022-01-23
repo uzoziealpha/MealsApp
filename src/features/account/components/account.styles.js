2e80e93660e16ccd8890ea6ea7162b1f25026d4f
Obinna Uzozie <Uzozie.obi@icloud.com>
2022-01-24 07:28:17 +0800

    includedlogin and registration buttons on drop screen

--- src/features/account/components/account.styles.js
+++ src/features/account/components/account.styles.js
@@ -1,4 +1,7 @@
 import styled from "styled-components/native";
+import { colors } from "../../../infrastructure/theme/colors";
+import { Button } from "react-native-paper";
+
 
 export const AccountBackground = styled.ImageBackground.attrs({
     source: require("../../../../assets/5home_bg.jpeg"),
@@ -12,5 +15,18 @@ export const AccountCover = styled.View`
     position: absolute;
     width: 100%;
     height: 100%;
-    background-color: rgba(255, 255, 255, 0.2);
+    background-color: rgba(255, 255, 255, 0.1);
 `;
+
+export const AccountContainer = styled.View`
+        background-color: rgba(255, 255, 255, 0.7);
+        padding: ${(props) => props.theme.space[4]};
+        margin-top: ${(props) => props.theme.space[2]};
+
+`;
+
+export const AuthButton = styled(Button).attrs({
+    color: colors.brand.primary,
+ })`
+   padding: ${(props) => props.theme.space[2]};
+ `;
--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -1,12 +1,33 @@
 // users entry screen to register or login
 import React from "react";
+import { AccountBackground, AccountContainer, AccountCover, AuthButton  } from "../components/account.styles";
+import { Spacer } from "../../../components/spacer/spacer.component";
 
-import { AccountBackground, AccountCover } from "../components/account.styles";
 
-export const AccountScreen = () => {
+
+
+export const AccountScreen = ({ navigation}) => {
   return (
     <AccountBackground>
      <AccountCover/>
+     <AccountContainer>
+         <AuthButton
+           icon="lock-open-outline"
+           mode="contained"
+           onPress={() => navigation.navigate("Login")}
+         >
+           Login 
+         </AuthButton>
+         <Spacer  size="large">
+         <AuthButton
+           icon="lock-open-outline"
+           mode="contained"
+           onPress={() => navigation.navigate("Register")}
+         >
+           Register
+         </AuthButton>
+         </Spacer>
+     </AccountContainer>
   </AccountBackground>
   );
 }
